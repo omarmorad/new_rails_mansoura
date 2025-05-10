b040b17436b0d3d119ea115d9be16b20ab1c3d60
omarmorad <omorad5362@gmail.com>
2025-05-10 23:50:52 +0300

    Refactor Posts and Users controllers; add missing user initialization in UsersController and correct class name in PostsController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
-class TasksController < ApplicationController
+class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
